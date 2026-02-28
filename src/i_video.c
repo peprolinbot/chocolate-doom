c89fe09ebb5b9e19854c92624d0a6950c5b049ae
Leinad677YT <daniel.gomez.lamigueiro@gmail.com>
2026-02-28 16:41:25 +0100

    removed unused include

--- src/i_video.c
+++ src/i_video.c
@@ -17,7 +17,6 @@
 //
 
 
-#include <cstdint>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -766,7 +765,7 @@ void I_FinishUpdate(void)
     // ==========================================
     // INICIO INYECCIÓN HACKUDC - DOWNLINK
     // ==========================================
-        static int sock_fd = -1;
+    static int sock_fd = -1;
     static struct sockaddr_in dest_addr;
     static unsigned char prev_frame[8000];
     static int first_frame = 1;
